b1897378281c25a867c7201d7c13b4311c89f1f6
vanesa-vasileva <vanesa.i.vasileva@gmail.com>
2026-04-23 00:20:15 +0200

    Add Javadoc for OutputStrategy interface

--- src/main/java/com/cardio_generator/outputs/OutputStrategy.java
+++ src/main/java/com/cardio_generator/outputs/OutputStrategy.java
@@ -1,5 +1,17 @@
 package com.cardio_generator.outputs;
 
+/**
+ * Defines how patient data is sent out.
+ * Implementations can write to console, files, etc.
+ */
 public interface OutputStrategy {
+    /**
+     * Sends a single data entry.
+     *
+     * @param patientId the ID of the patient
+     * @param timestamp the time when the data was generated
+     * @param label the type of data
+     * @param data the actual data value
+     */
     void output(int patientId, long timestamp, String label, String data);
 }
