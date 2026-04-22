ce5716243ae70fac4144ea4345b86bf8fbe6d72f
vanesa-vasileva <vanesa.i.vasileva@gmail.com>
2026-04-23 00:14:58 +0200

    Add Javadoc for PatientDataGenerator interface

--- src/main/java/com/cardio_generator/generators/PatientDataGenerator.java
+++ src/main/java/com/cardio_generator/generators/PatientDataGenerator.java
@@ -2,6 +2,16 @@ package com.cardio_generator.generators;
 
 import com.cardio_generator.outputs.OutputStrategy;
 
+/**
+ * Represents a data generator for a single patient.
+ * Each generator produces a specific type of health data.
+ */
 public interface PatientDataGenerator {
+    /**
+     * Generates health data for a patient and sends it to the output.
+     *
+     * @param patientId the ID of the patient
+     * @param outputStrategy where to send the generated data
+     */
     void generate(int patientId, OutputStrategy outputStrategy);
 }
